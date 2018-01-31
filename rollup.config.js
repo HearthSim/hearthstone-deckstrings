cc82a0d4d69871ad7e003e65f87d76545d84fa12
Benedict Etzel <benedict@hearthsim.net>
2018-01-31 20:22:18 +0100

    Build ESM file (index.mjs)

--- rollup.config.js
+++ rollup.config.js
@@ -3,20 +3,25 @@ import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript";
 import pkg from "./package.json";
 
-export default [
-	{
-		input: "src/index.ts",
-		output: {
+export default {
+	input: "src/index.ts",
+	output: [
+		{
 			name: "deckstrings",
-			file: pkg.main,
+			file: `${pkg.main}.js`,
 			format: "umd",
 		},
-		plugins: [
-			typescript({
-				typescript: require("typescript"),
-			}),
-			resolve(),
-			commonjs(),
-		],
-	},
-];
+		{
+			name: "deckstrings",
+			file: `${pkg.main}.mjs`,
+			format: "es",
+		},
+	],
+	plugins: [
+		typescript({
+			typescript: require("typescript"),
+		}),
+		resolve(),
+		commonjs(),
+	],
+};
