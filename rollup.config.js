a9f654afe34e92f95dfbe0ac77e193fac67e7792
Benedict Etzel <benedict@hearthsim.net>
2018-02-02 22:23:59 +0100

    Clean up rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,25 @@
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript";
+import tsc from "typescript";
 import pkg from "./package.json";
 
 export default {
 	input: "src/index.ts",
 	output: [
 		{
-			name: "deckstrings",
-			file: `${pkg.main}.js`,
 			format: "umd",
+			file: `${pkg.main}.js`,
+			name: "deckstrings",
 		},
 		{
-			name: "deckstrings",
-			file: `${pkg.main}.mjs`,
 			format: "es",
+			file: `${pkg.main}.mjs`,
 		},
 	],
 	plugins: [
 		typescript({
-			typescript: require("typescript"),
+			typescript: tsc,
 		}),
 		resolve(),
 		commonjs(),
