96c5994845b8313d76d6021818a169baeb276d05
sakai-akinobu <sakai.akinobu@gmail.com>
2019-02-23 13:57:55 +0900

    Use `rollup-plugin-typescript2` plugin.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import jscc from "rollup-plugin-jscc";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
-import typescript from "rollup-plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 import tsc from "typescript";
 import pkg from "./package.json";
 
